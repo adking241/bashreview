cbcbd591529ebee4c221ed355f7b69e9ebcf09f7
Macy <macyjackson@ymail.com>
2017-08-09 22:10:13 -0400

    so_close:testpass

--- bash.rb
+++ bash.rb
@@ -1,3 +1,14 @@
-def winners_or_losers(winning_numbers, my_ticket)
-	"winner"
-	end
+def winners_or_losers2(winning_numbers3, my_ticket4)
+	"winner1"
+	if winning_numbers3.include?(my_ticket4)
+	puts "winner1"
+	"winner1"
+	else
+	puts "loser1"
+	"loser1"
+	end		
+end
+
+def so_close(my_ticket4, winning_num)
+	false
+end
--- test.rb
+++ test.rb
@@ -8,8 +8,21 @@ class BashTest < Minitest::Test
 	end
 
 	def test_assert_to_see_if_you_won
-	my_ticket = "1234"
-	winning_numbers = ["1515", "1487", "1234"]
-	assert_equal("winner", winners_or_losers(winning_numbers, my_ticket))
+	my_ticket4 = "1234"
+	winning_numbers3 = ["1515", "1487", "1234"]
+	assert_equal("winner1", winners_or_losers2(winning_numbers3, my_ticket4))
 	end
+
+	def test_if_non_winning_ticket_returns_lost_string
+		my_ticket4 = "4444"
+	winning_numbers3 = ["1515", "1487", "1234"]
+	assert_equal("loser1", winners_or_losers2(winning_numbers3, my_ticket4))
+	end
+
+	def test_assert_if_my_ticket_is_so_close_false
+		my_ticket4 = "4444"
+		winning_num = "4443"
+		assert_equal(false, so_close(my_ticket4, winning_num))
+	end
+
 end
